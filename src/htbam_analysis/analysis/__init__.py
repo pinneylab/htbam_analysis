4c5694f9d005192f13c73cffc1e93938c1300fca
Duncan Muir <43482819+duncan-muir@users.noreply.github.com>
2024-05-03 17:26:30 -0700

    add experiment and plotting module

---
+++ src/htbam_analysis/analysis/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for analysis."""
+
+from htbam_analysis.analysis import experiment, plotting
+
+__author__ = """Duncan Muir"""
+__email__ = ""
+__version__ = "0.1.0"
